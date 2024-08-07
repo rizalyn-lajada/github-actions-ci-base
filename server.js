7cd8ea4cc4df67b70b284e9f365d470b2f23e6c7
rizalynlajada <rizalynlajada@gmail.com>
2024-08-06 22:14:36 -0600

    Update server response with SAIT ID

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0'; // This allows the server to bind to all network interfaces
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello I am Rizalyn Lajada, SAIT ID 000905612\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello I am Rizalyn Lajada, SAIT ID 000905612. This is the project 3 on Virtualization.\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
+
