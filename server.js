a59ee81fcf350c9e74979dfb2f826b0db13b007e
rizalynlajada <rizalynlajada@gmail.com>
2024-08-07 14:50:49 -0600

    Update server response with SAIT ID

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello I am Rizalyn Lajada, SAIT ID 000905612.\n');
+    res.end('Hello I am Rizalyn Lajada, SAIT ID 000905612.I am in my 3rd semester now.\n');
 });
 
 server.listen(port, hostname, () => {
